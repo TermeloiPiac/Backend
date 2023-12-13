fd883ebea70fb75f88e620de8c48e596baea7690
Darci01 <adam.dara@hotmail.com>
2023-12-13 21:43:47 +0100

    init project

---
+++ termeloiPiac/src/main/java/com/termeloiPiac/TermeloiPiacApplication.java
@@ -0,0 +1,13 @@
+package com.termeloiPiac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermeloiPiacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermeloiPiacApplication.class, args);
+	}
+
+}
---
+++ termeloiPiac/src/test/java/com/termeloiPiac/TermeloiPiacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.termeloiPiac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermeloiPiacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
